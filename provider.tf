aba4e455a4a00d5e5c26fc844850c56980106460
aget2101 <138261223+aget2101@users.noreply.github.com>
2024-02-15 14:27:07 -0600

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -1,4 +1,12 @@
-
+terraform {
+  backend "s3" {
+    bucket = "abreham-terraform-state"
+    key    = "global/s3/terraform.tfstate"
+    region = "us-west-2"
+    dynamo_table = "terraform-state"
+    encrypt = true
+  }
+}
 
 # Configure the AWS Provider
 provider "aws" {
